14a7a29e905b59b7376b1902fce97aaeb2da3f89
Ante <136327732+anteb03@users.noreply.github.com>
2023-10-19 21:11:21 +0200

    Delete Vjezba3 directory

--- Vjezba3/zadatak1.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef ZADATAK1_HPP
-#define ZADATAK1_HPP
-
-#include <vector>
-void unossabrojemel(std::vector<int>& vektor, int brojElemenata);
-void ispisvek(const std::vector<int>& vektor);
-void unosveksintervalom(std::vector<int>& vektor, int donjaGranica, int gornjaGranica);
-#endif
--- Vjezba3/zadatak2.hpp
+++
@@ -1,5 +0,0 @@
-#ifndef ZADATAK2_HPP
-#define ZADATAK2_HPP
-#include <vector>
-void razlikavek(const std::vector<int>& vektor1, const std::vector<int>& vektor2, std::vector<int>& rezultat);
-#endif
