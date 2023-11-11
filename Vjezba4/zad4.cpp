95ab88da08de9a56cffd7a023d2afa8ee0ea7cb7
Ante <136327732+anteb03@users.noreply.github.com>
2023-11-11 13:52:12 +0100

    Update zad4.cpp

--- Vjezba4/zad4.cpp
+++ Vjezba4/zad4.cpp
@@ -1,72 +1,56 @@
-﻿#include <iostream>
+#include <iostream>
 #include <string>
 #include <vector>
 #include <ctime>
 #include <cstdlib>
 
-std::string fuja(const std::vector<std::string>& recenice) {
-    std::srand(static_cast<unsigned>(std::time(nullptr)));
-    int random = std::rand() % recenice.size();
-    std::string izabrana = recenice[random];
-    std::cout << izabrana << std::endl;
-    std::string piglatinrečenica = "";
-    std::string word = "";
-    for (char c : izabrana) {
-        if (std::isalpha(c)) {
-            word += c;
-        }
-        else {
-            if (!word.empty()) {
-                std::string piglatinword = "";
-                char prvi = std::tolower(word[0]);
-                if (prvi == 'a' || prvi == 'e' || prvi == 'i' || prvi == 'o' || prvi == 'u') {
-                    piglatinword = word + "hay";
-                }
-                else {
-                    int index = 0;
-                    while (index < word.size() && !std::strchr("aeiouAEIOU", word[index])) {
-                        index++;
-                    }
-                    if (index < word.size()) {
-                        piglatinword = word.substr(index) + word.substr(0, index) + "ay";
-                    }
-                    else {
-                        piglatinword = word + "ay";
-                    }
-                }
-                piglatinrečenica += piglatinword + c;
-                word = "";
-            }
-            else {
-                piglatinrečenica += c;
-            }
-        }
-    }
-    if (!word.empty()) {
-        std::string piglatinword = "";
-        char prvi = std::tolower(word[0]);
-        if (prvi == 'a' || prvi == 'e' || prvi == 'i' || prvi == 'o' || prvi == 'u') {
-            piglatinword = word + "hay";
-        }
-        else {
-            int index = 0;
-            while (index < word.size() && !std::strchr("aeiouAEIOU", word[index])) {
-                index++;
-            }
-            if (index < word.size()) {
-                piglatinword = word.substr(index) + word.substr(0, index) + "ay";
-            }
-            else {
-                piglatinword = word + "ay";
-            }
-        }
-        piglatinrečenica += piglatinword;
-    }
-    return piglatinrečenica;
+bool isVowel(char c) {
+    c = tolower(c);
+    return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
 }
+
+std::string convertToPigLatin(const std::string& word) {
+    std::string pigLatinWord = word;
+    char first = tolower(pigLatinWord[0]);
+
+    if (isVowel(first)) {
+        pigLatinWord += "hay";
+    }
+    else {
+        size_t firstVowelPos = 0;
+        for (size_t i = 1; i < pigLatinWord.length(); i++) {
+            if (isVowel(pigLatinWord[i])) {
+                firstVowelPos = i;
+                break;
+            }
+        }
+        pigLatinWord = pigLatinWord.substr(firstVowelPos) + pigLatinWord.substr(0, firstVowelPos) + "ay";
+    }
+    return pigLatinWord;
+}
+
 int main() {
-    std::vector<std::string> recenice = { "Hhello, wrorld!","Wjjat time sis it?","This is a ttest sqentence." };
-    std::string piglatin = fuja(recenice);
-    std::cout << piglatin << std::endl;
+    std::vector<std::string> sentences = {
+        "Ovo ej prv recenica.",
+        "Evo druge recenice za prijevod.",
+        "Treća recenica za testiranje programa."
+    };
+    std::srand(static_cast<unsigned int>(std::time(nullptr)));
+    int randomIndex = std::rand() % sentences.size();
+    std::string randomstring = sentences[randomIndex];
+    std::string pigLatinResult;
+    size_t start = 0;
+    size_t end = 0;
+    while (end != std::string::npos) {
+        end = randomstring.find_first_of(" .,!?;:", start);
+        std::string word = randomstring.substr(start, end - start);
+        if (!word.empty()) {
+            std::string pigLatinWord = convertToPigLatin(word);
+            pigLatinResult += pigLatinWord +  " ";
+        }
+        start = end + 1;
+    }
+    std::cout << "Izabrana: " << randomstring << std::endl;
+    std::cout << "Prijevod: " << pigLatinResult << std::endl;
     return 0;
 }
